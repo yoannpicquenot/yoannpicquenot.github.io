70467485440c14976f4a6d797bac3054ff99a394
Yoann Picquenot <yoann.picquenot.prestataire@sfr.com>
2019-06-20 14:17:02 +0200

    Add new experience

--- public/js/main.js
+++ public/js/main.js
@@ -34,56 +34,14 @@ $(document).ready(function() {
                 top < $('#skills').offset().top) {
                 $('nav ul li .underline').removeClass('active');
                 $('nav ul li .experiences-underline').addClass('active');
-            } else if (top >= $('#skills').offset().top &&
-                top < $('#contact').offset().top) {
+            } else if (top >= $('#skills').offset().top) {
                 $('nav ul li .underline').removeClass('active');
                 $('nav ul li .skills-underline').addClass('active');
-            } else if (top >= $('#contact').offset().top) {
-                $('nav ul li .underline').removeClass('active');
-                $('nav ul li .contact-underline').addClass('active');
             } else {
                 $('nav ul li .underline').removeClass('active');
             }
         });
     }
-
-    function initForm() {
-        console.log(window.localStorage.getItem('liamesahneebtnes'));
-        if(window.localStorage.getItem('liamesahneebtnes') === 'true') {
-            $('#contact-form').hide();
-            $('.form-message').css('opacity', '1');
-        } else {
-            $('#contact-form').show();
-            $('#contact-form').submit(sendForm);
-        }
-    }
-
-    function sendForm(e) {
-        var data = {};
-        $('.button-send-form').text('SENDING...');
-        e.preventDefault();
-        $(e.target.children).each(function(_, el) {
-            var d = $(el).children()[0];
-            if (typeof(d) !== 'undefined') {
-                data[$(d).attr('name')] = d.value;
-            }
-        });
-        $.ajax({
-            method: 'POST',
-            url: '/email',
-            data: JSON.stringify(data),
-            contentType: 'application/json'
-        }).done(function(res) {
-            if(res.status === 200) {
-                $('form').animate({opacity: 0}, 'slow', function() {
-                    $('form').hide();
-                    $('.form-message').animate({opacity: 1}, 'slow');
-                    window.localStorage.setItem('liamesahneebtnes', true);
-                });
-            }
-        });
-    }
-
     $('.button.goUp').on('click', function() {
         $('html, body').stop().animate({
             scrollTop: 0
@@ -92,7 +50,6 @@ $(document).ready(function() {
     });
     setNavLink();
     initScrollSpy();
-    initForm();
     var top = $(window).scrollTop();
     if (top > 110) {
         $('.button.goUp').css('opacity', top / 200);
